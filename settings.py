131a94faf4a3ba8e141d149ca4d812d9411cc3cf
user <filia150@mail.ru>
2025-05-07 00:29:10 +0300

    add db and cache settings

--- settings.py
+++ settings.py
@@ -3,4 +3,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # GOOGLE_TOKEN_ID: str = "hhf43u3hqto438hgo784u"
-    sqlite_db_name: str = 'pomodoro_db.sqlite'
+    # sqlite_db_name: str = 'pomodoro_db.sqlite'
+    DB_HOST: str = 'localhost'
+    DB_PORT: int = 5431
+    DB_USER: str = 'docker_postgres'
+    DB_PASSWORD: str = 'docker_postgres'
+    DB_NAME: str = 'docker_postgres'
+    CACHE_HOST: str = 'localhost'
+    CACHE_PORT: int = 6378
+    CACHE_DB: int = 0
